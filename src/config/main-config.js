88514001fb452df8837278b080a30a9789b80d68
Cristian Perez <cristiangp777@gmail.com>
2019-03-20 13:46:03 -0700

    checkpoint 2 complete

---
+++ src/config/main-config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const logger = require('morgan');
+const bodyParser = require("body-parser");
+
+module.exports = {
+  init(app, express){
+     app.use(logger('dev'));
+    app.use(bodyParser.urlencoded({ extended: true }));
+}
+
+};
