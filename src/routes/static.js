df92af0ed19504cf2c5213bc2cfa6f4e98a3acb2
Cristian Perez <cristiangp777@gmail.com>
2019-03-20 18:38:11 -0700

    temp

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const staticController = require("../controllers/staticController");
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccipedia");
-});
-
+ router.get("/", staticController.index);
 module.exports = router;
